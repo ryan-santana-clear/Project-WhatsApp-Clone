499adbd0fc1de7e7d0b3b2692d9cd5753bdb005a
Ryan Santana <ryan.santana.clear@gmail.com>
2023-03-08 12:15:06 -0300

    Feat: Creating the Prototypes

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController {
 
         console.log('WhatsAppController 0K');
 
+        this.elementsPrototype();
         this.loadElements();
 
     }
@@ -20,4 +21,58 @@ class WhatsAppController {
 
     }
 
+    elementsPrototype(){
+
+        Element.prototype.hide = function() {
+            this.style.display = 'none';
+            return this;
+        }
+
+        Element.prototype.show = function() {
+            this.style.display = 'block';
+            return this;
+        }
+
+        Element.prototype.toggle = function() {
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+            return this;
+        }
+
+        Element.prototype.on = function(events, fn) {
+            events.split(' ').forEach(event=>{
+                this.addEventListener(event, fn);
+            });
+            return this;
+        }
+
+        Element.prototype.css = function(styles){
+            for (let name in styles) {
+                this.style[name] = styles[name];
+            }
+            return this;
+        }
+
+        Element.prototype.addClass = function(name) {
+            this.classLis.add(name);
+            return this;
+        }
+        Element.prototype.removeClass = function(name) {
+            this.classList.remove(name);
+            return this;
+        }
+        Element.prototype.toggleClass = function(name) {
+            this.classList.toggle(name);
+            return this;
+        }
+        Element.prototype.hasClass = function(name) {
+            return this.classList.contains(name);
+        }
+
+    }
+
 }
+
+
+
+
+
