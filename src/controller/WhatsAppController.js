616e4dbeb39dc3607a09dfbd8ccb9a514c8c9d2d
Ryan Santana <ryan.santana.clear@gmail.com>
2023-03-08 13:03:13 -0300

    Feat: Getting data from panels and using it with FormData

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -57,16 +57,39 @@ class WhatsAppController {
             this.classList.add(name);
             return this;
         }
+        
         Element.prototype.removeClass = function(name) {
             this.classList.remove(name);
             return this;
         }
+        
         Element.prototype.toggleClass = function(name) {
             this.classList.toggle(name);
             return this;
         }
+        
         Element.prototype.hasClass = function(name) {
             return this.classList.contains(name);
+        }
+
+        HTMLFormElement.prototype.getForm = function () {
+
+            return new FormData(this);
+
+        }
+        
+        HTMLFormElement.prototype.toJSON = function () {
+
+            let json = {};
+
+            this.getForm().forEach((value, k)=>{
+
+                json[key] = value;
+
+            });
+
+            return json;
+
         }
 
     }
@@ -103,6 +126,34 @@ class WhatsAppController {
         this.el.btnClosePanelAddContact.on('click', e=>{
 
             this.el.panelAddContact.removeClass('open');
+
+        });
+
+        this.el.photoContainerEditProfile.on('click', e=>{
+
+            this.el.inputProfilePhoto.click();
+
+        });
+        
+        this.el.inputNamePanelEditProfile.on('keypress', e=>{
+
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e=>{
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
+        });
+
+        this.el.formPanelAddContact.on('submit', e=>{
+
+            e.preventDefault();
+
+            let formData = new FormData(this.el.formPanelAddContact);
 
         });
 
