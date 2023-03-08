df5d3d641ddf323af3906c491ed352eb7d07d030
Ryan Santana <ryan.santana.clear@gmail.com>
2023-03-08 12:37:49 -0300

    Feat: Implementing event to show left side panels

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -6,6 +6,7 @@ class WhatsAppController {
 
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
 
     }
 
@@ -53,7 +54,7 @@ class WhatsAppController {
         }
 
         Element.prototype.addClass = function(name) {
-            this.classLis.add(name);
+            this.classList.add(name);
             return this;
         }
         Element.prototype.removeClass = function(name) {
@@ -67,6 +68,50 @@ class WhatsAppController {
         Element.prototype.hasClass = function(name) {
             return this.classList.contains(name);
         }
+
+    }
+
+    initEvents(){
+
+        this.el.myPhoto.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+            
+
+        });
+
+        this.el.btnNewContact.on('click', e =>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e =>{
+
+            this.el.panelEditProfile.removeClass('open');
+
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e=>{
+
+            this.el.panelAddContact.removeClass('open');
+
+        });
+
+    }
+
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
 
     }
 
