6b71e579c0c1280078b516cc3aa6767904ba5b34
Ryan Santana <ryan.santana.clear@gmail.com>
2023-03-14 12:59:04 -0300

    Feat: Adding the list of contacts registered by the user

--- src/model/User.js
+++ src/model/User.js
@@ -49,6 +49,14 @@ export class User extends Model {
 
     }
 
+    static getContactsRef(id) {
+
+        return User.getRef()
+        .doc(id)
+        .collection('contacts')        
+
+    }
+
     static findByEmail(email){
 
         return User.getRef().doc(email);
@@ -57,11 +65,37 @@ export class User extends Model {
 
     addContact(contact){
 
-        return User.getRef()
-        .doc(this.email)
-        .collection('contacts')
+        return User.getContactsRef(this.email)
         .doc(btoa(contact.email))
         .set(contact.toJSON());
+
+    }
+
+    getContacts(){
+
+        return new Promise((s, f)=>{
+
+            User.getContactsRef(this.email).onSnapshot(docs => {
+
+                let contacts = [];
+
+                docs.forEach(doc => {
+
+                    let data = doc.data();
+
+                    data.id = doc.id;
+
+                    contacts.push(data);
+                    
+                });
+
+                this.trigger('contactschange', docs);
+
+                s(contacts);
+
+            });
+
+        });
 
     }
 
