af229887c04a0fcf152d91427c21d74d1d7d8204
Ryan Santana <ryan.santana.clear@gmail.com>
2023-03-14 11:44:02 -0300

    Feat: Editing and updating user profile data

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -197,9 +197,17 @@ export class WhatsAppController {
             }
         });
 
-        this.el.btnSavePanelEditProfile.on('click', e=>{
+        this.el.btnSavePanelEditProfile.on('click', e => {
 
-            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+            this.el.btnSavePanelEditProfile.disabled = true;
+
+            this._user.name = this.el.inputNamePanelEditProfile.innerHTML;
+
+            this._user.save().then(()=>{
+
+                this.el.btnSavePanelEditProfile.disabled = false;
+
+            });
 
         });
 
